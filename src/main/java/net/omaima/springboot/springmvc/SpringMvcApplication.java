f08b70b09bab415a24117ff84728375c2429fc98
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-03-09 11:25:58 +0000

    ading navbar

--- src/main/java/net/omaima/springboot/springmvc/SpringMvcApplication.java
+++ src/main/java/net/omaima/springboot/springmvc/SpringMvcApplication.java
@@ -6,9 +6,7 @@ import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 //import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication()
