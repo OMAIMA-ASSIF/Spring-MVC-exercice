d7d33ec85e674b7ca65d0c5c8f1f19f6ebc436b5
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-03-16 21:53:52 +0000

    Security Configuration- roles and paths

--- src/main/java/net/omaima/springmvcspringsecurity/web/ProductController.java
+++ src/main/java/net/omaima/springmvcspringsecurity/web/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("productsList", products);
@@ -26,26 +26,26 @@ public class ProductController {
     }
     @GetMapping("/")
     public String home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/newProduct")
+    @GetMapping("/admin/newProduct")
     public String newProduct(Model model){
         model.addAttribute("product", new Product());
         return "new-product";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
         return "redirect:/index";
     }
 
-    @PostMapping("/saveProduct")
+    @PostMapping("/admin/saveProduct")
     public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model){
         if(bindingResult.hasErrors()){return "new-product";}
         productRepository.save(product);
-        return "redirect:/newProduct";
+        return "redirect:/admin/newProduct";
     }
 }
 
