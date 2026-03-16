2d1534cc199d26dd0b9502b6072a8533dd18f27f
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-03-16 18:57:51 +0000

    Adding new product form

--- src/main/java/net/omaima/springmvcspringsecurity/web/ProductController.java
+++ src/main/java/net/omaima/springmvcspringsecurity/web/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -25,9 +26,22 @@ public class ProductController {
     public String home(){
         return "redirect:/index";
     }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "new-product";
+    }
+
     @GetMapping("/delete")
     public String delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(Product product){
+        productRepository.save(product);
         return "redirect:/index";
     }
 }
