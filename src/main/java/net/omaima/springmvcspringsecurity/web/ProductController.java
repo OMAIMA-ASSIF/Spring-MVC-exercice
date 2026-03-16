a27ab4e7256d708867df8cee575a7ca2239a6001
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-03-16 15:46:43 +0000

    adding bootstrap

---
+++ src/main/java/net/omaima/springmvcspringsecurity/web/ProductController.java
@@ -0,0 +1,24 @@
+package net.omaima.springmvcspringsecurity.web;
+
+import net.omaima.springmvcspringsecurity.entities.Product;
+import net.omaima.springmvcspringsecurity.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productsList", products);
+        return "products";
+    }
+
+}
