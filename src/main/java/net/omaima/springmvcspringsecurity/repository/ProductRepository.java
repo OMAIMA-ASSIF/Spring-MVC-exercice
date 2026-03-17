79baa91cd5bf5f17e30cbac592d08bccc7d3b484
Omaima Assif <omaima.assif-etu@etu.univh2c.ma>
2026-03-17 22:49:21 +0000

    Update ProductRepository.java

--- src/main/java/net/omaima/springmvcspringsecurity/repository/ProductRepository.java
+++ src/main/java/net/omaima/springmvcspringsecurity/repository/ProductRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // This allows searching for products where the name contains the keyword (case insensitive)
     List<Product> findByNameContainingIgnoreCase(String keyword);
+    
 }
